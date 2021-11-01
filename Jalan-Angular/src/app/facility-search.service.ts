c773bf786f2a182bb13e4c4246c3d08d9d5febd8
前田 <sloth19920731@gmail.com>
2021-11-01 23:49:18 +0900

    Django用にStepfunctions向けの処理削除

--- Jalan-Angular/src/app/facility-search.service.ts
+++ Jalan-Angular/src/app/facility-search.service.ts
@@ -3,15 +3,12 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 const apigatewayURL: string = environment.apigatewayURL;
-const httpOptions = {
-  headers: { 'Content-Type': 'application/json' }
-};
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacilitySearchService {
-  execTaskName = '';
   constructor(private http: HttpClient) { }
 
    /**
@@ -19,24 +16,5 @@ export class FacilitySearchService {
    */
     getFacilities(keyword: String): Observable<any> {
       return this.http.get(`${apigatewayURL}${keyword}`);
-    }
-
-
-    exec_task(keyword: String): Observable<any> {
-      const reqbody = {
-        input: `{"keyword": ${keyword}}`,
-        name: 'MyExecution_',
-        stateMachineArn:
-          'arn:aws:states:ap-northeast-1:473405973194:stateMachine:JalanMachine'
-      };
-      return this.http.post<any>(apigatewayURL, reqbody, httpOptions);
-    }
-  
-    status_task(): Observable<any> {
-      const reqbody = {
-        executionArn: this.execTaskName
-      };
-      const statusUrl = apigatewayURL + '/status';
-      return this.http.post<any>(statusUrl, reqbody, httpOptions);
     }
 }
