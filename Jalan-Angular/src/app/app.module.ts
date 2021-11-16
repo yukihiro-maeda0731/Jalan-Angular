e85538314094177a117855877b67efb958143e72
前田 <sloth19920731@gmail.com>
2021-11-16 23:48:25 +0900

    一番上にスクロールするfloating action button作成

--- Jalan-Angular/src/app/app.module.ts
+++ Jalan-Angular/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CommentsComponent } from './comments/comments.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     BrowserAnimationsModule,
     MatProgressSpinnerModule,
-    OverlayModule
+    OverlayModule,
+    MatButtonModule
   ],
   entryComponents: [
     MatSpinner
