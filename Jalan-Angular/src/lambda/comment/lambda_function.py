bd6c2823daba8680dc8b25cf0960aa30f86f3896
前田 <sloth19920731@gmail.com>
2022-10-06 20:09:05 +0900

    スクレイピング先の構造変わったので対応

--- Jalan-Angular/src/lambda/comment/lambda_function.py
+++ Jalan-Angular/src/lambda/comment/lambda_function.py
@@ -2,47 +2,56 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.parse
 import json
-
+import re
 
 def lambda_handler(event,context):
-    facilityNo = event['queryStringParameters']['facilityNo']
+    keyword = event['queryStringParameters']['keyword']
     currentIndex = event['queryStringParameters']['currentIndex']
-    urlFacilityNo = urllib.parse.quote(facilityNo, encoding='shift-jis')
-    
-    url = f"https://www.jalan.net/yad{urlFacilityNo}/kuchikomi/?yadNo={urlFacilityNo}"
-    # 2ページ目以降はurlのルールが異なる
-    if not currentIndex == '1':
-        url = f"https://www.jalan.net/yad{urlFacilityNo}/kuchikomi/{currentIndex}.HTML?yadNo={urlFacilityNo}"
-        
+    urlKeyword = urllib.parse.quote(keyword, encoding='shift-jis')
+    url = f"https://www.jalan.net/uw/uwp2011/uww2011init.do?keyword={urlKeyword}&dispStartIndex={currentIndex}"
+    print("url")
+    print(url)
     r = requests.get(url)
     c = r.content
+    print("c")
+    print(c)
     # ここ原因でAPIGatewayがtimeoutなる
     # soup = BeautifulSoup(c, "html.parser", from_encoding="Shift_JIS")
+    # これだとタイムアウト回避できるがデータが少なくなってしまう
     # soup = BeautifulSoup(c, "lxml", from_encoding="Shift_JIS")
+    # utf-8に変換してからパース
     soup = BeautifulSoup(c.decode("CP932"), "lxml")
-    all=soup.find_all("p",{"class":"jlnpc-kuchikomiCassette__postBody"})
-    
-    starRatingGroup = soup.find_all("div",{"class":"jlnpc-kuchikomiCassette__totalRate"})
-    starRatingGroupIndex = 0
-    
-    commentTitleGroup = soup.find_all("p",{"class":"jlnpc-kuchikomiCassette__lead"})
-    commentTitleGroupIndex = 0
-
-    comments=[]
+    all = soup.find_all("div",{"class":"p-yadoCassette__body p-searchResultItem__body"})
+    facilityName = soup.find_all("h2",{"class":"p-searchResultItem__facilityName"})
+    facilityNameIndex = 0
+    addressGroup = soup.find_all("dd",{"class":"p-searchResultItem__facilityInfoValue"})
+    addressGroupIndex = 0
+    facilities = []
+    print("a")
+    print(all)
 
     for item in all:
-        d={}
-        d["comment"]=item.text
-        d["starRating"]=starRatingGroup[starRatingGroupIndex].text
-        d["commentTitle"]=commentTitleGroup[commentTitleGroupIndex].text
-        starRatingGroupIndex = starRatingGroupIndex + 1
-        commentTitleGroupIndex = commentTitleGroupIndex + 1
-        comments.append(d)
+        d = {}
+        print("itemtext")
+        print(item.text)
+        d["facilityName"] = facilityName[facilityNameIndex].text
+        facilityNameIndex = facilityNameIndex + 1
+        link = item.find('a')
+        print("link")
+        print(link)
+        # reで文字列から数字以外を削除
+        d["facilityNo"] = re.sub(r"\D", "", link.get('href'))[0:6]
+        d["facilityAddress"] = addressGroup[addressGroupIndex].text
+        # 偶数が必要な情報(住所)・奇数が不要な情報のため偶数のみ格納
+        addressGroupIndex = addressGroupIndex + 2
+        facilities.append(d)
+    print("facilitiesの前")
+    print(facilities)
 
     return  {
         'statusCode': 200,
         'headers': {
             "Access-Control-Allow-Origin": "*"
         },
-        'body': json.dumps(comments)
+        'body': json.dumps(facilities)
     }
